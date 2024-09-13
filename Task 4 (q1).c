e4c04826380b010d334d4bf06fd6008508c0e6de
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-09-13 14:24:57 +0500

    Create Task 4 (q1).c

---
+++ Task 4 (q1).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	int Score;
+	printf("Enter your score: ");
+	scanf("%d",&Score);
+	
+	if (Score <=100 && Score >= 90){
+		printf("A Grade");
+	}else if (Score <=89 && Score >= 80){
+		printf("B Grade");
+	}else if (Score <=79 && Score >= 70){
+		printf("C Grade");
+	}else if (Score <=69 && Score >= 60){
+		printf("D Grade");
+	}else if (Score <=60){
+		printf("F Grade");
+	}else{
+		printf("Invalid Input");
+	}
+	return 0;
+}
