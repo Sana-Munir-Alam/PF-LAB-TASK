a334b30021e6351fcbe952c7aac3b80d2aa80a74
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-10-11 15:35:36 +0500

    Create LAB 7 Q5.c

---
+++ Task 7/LAB 7 Q5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int sizeArray;
+	int Array[20];
+	for (int i=0; i <= 20; i++){
+		printf("Enter Array[%d] value: ", i);
+		scanf("%d", & Array[i]);
+	}
+	sizeArray = sizeof(Array)/sizeof(Array[0]);
+	printf("The Reverse Order of your Array is: ");
+	for (int i = 20; i>=0; i--){
+		printf("%d, ",Array[i]);
+	}
+	return 0;
