e3817e1123389fdbe9fb4f37b95b366801b0a285
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-10-11 15:36:09 +0500

    Create LAB 7 Q6.c

---
+++ Task 7/LAB 7 Q6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	int Max, Min;
+	int sizeArray;
+	int Array[30];
+	for (int i=0; i <= 30; i++){
+		printf("Enter Array[%d] value: ", i);
+		scanf("%d", & Array[i]);
+	}
+	Min = Array[0];
+	Max = Array[0];
+	sizeArray = sizeof(Array)/sizeof(Array[0]);
+	for (int i = 0; i<=sizeArray; i++){
+		if (Array[i] > Max){
+			Max = Array[i];
+		}
+		if (Array[i] < Min){
+			Min = Array[i];
+		}
+	}
+	printf("The Max value is: %d\n", Max);
+	printf("the Min value is: %d", Min);
+	return 0;
+}
