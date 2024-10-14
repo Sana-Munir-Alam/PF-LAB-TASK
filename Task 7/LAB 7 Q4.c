1528a07fb7aabe6d52cde98fad050620cdfd631f
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-10-14 08:30:02 -0700

    Update LAB 7 Q4.c

--- Task 7/LAB 7 Q4.c
+++ Task 7/LAB 7 Q4.c
@@ -3,12 +3,12 @@ int main(){
 	int sum;
 	int sizeArray;
 	int Array[15];
-	for (int i=0; i <= 15; i++){
+	for (int i=0; i < 15; i++){
 		printf("Enter Array[%d] value: ", i);
 		scanf("%d", & Array[i]);
 	}
 	sizeArray = sizeof(Array)/sizeof(Array[0]);
-	for (int i = 0; i<=sizeArray; i++){
+	for (int i = 0; i< sizeArray; i++){
 		sum = sum + Array[i];
 	}
 	printf("The sum of your given array is: %d", sum);
