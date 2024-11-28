91b1c1f1bebed0ea80fd3bca0c740a6c1065a62d
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-11-28 06:04:56 -0800

    Create Task 2.c

---
+++ Task 11/Task 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *file;
+    char FileName[100];
+    char line[256];
+    int LineNumber = 1;
+
+    printf("\nEnter the FileName to open (e.g Animal.txt): ");
+    scanf("%s", FileName);
+    file = fopen(FileName, "r");
+    if (file == NULL) {
+        printf("Error: Could not open file %s\n", FileName);
+        return 1;
+    }
+    printf("Contents of the file with line numbers:\n\n");
+    while (fgets(line, sizeof(line), file)) {
+        printf("%d: %s", LineNumber, line);
+        LineNumber++;
+    }
+    fclose(file);
+    return 0;
+}
